11f3e887eb2fc4a5eccae83c7aef831d22f1a218
Penelope Gwen <pogmom@server-dell-p95g001.local>
2023-11-17 15:55:03 -0700

    first commit

---
+++ wyoming_piper/docker.sh
@@ -0,0 +1,1 @@
+docker run --restart=always -it -p 10200:10200 -v /home/pogmom/Dockers/wyoming_piper/data:/data rhasspy/wyoming-piper --voice en-us-libritts-high
---
+++ wyoming_whisper/docker.sh
@@ -0,0 +1,1 @@
+docker run --restart=always -it -p 10300:10300 -v /home/pogmom/Dockers/wyoming_whisper/data:/data rhasspy/wyoming-whisper --model tiny-int8 --language en
