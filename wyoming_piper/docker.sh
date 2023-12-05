50198bb918888697dbfa9efac16084bd8ee7d9bb
Penelope Gwen <pogmommypenny@gmail.com>
2023-12-04 20:21:14 -0700

    add some documentation in comments

--- wyoming_piper/docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --restart=always -it -p 10200:10200 -v /home/pogmom/Dockers/wyoming_piper/data:/data rhasspy/wyoming-piper --voice en-us-libritts-high
--- wyoming_whisper/docker.sh
+++
@@ -1,1 +0,0 @@
-docker run --restart=always -it -p 10300:10300 -v /home/pogmom/Dockers/wyoming_whisper/data:/data rhasspy/wyoming-whisper --model tiny-int8 --language en
