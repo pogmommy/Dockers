56b9b4ec885ca059af00253727bd346f4ba68a48
Penelope Gwen <pogmom@server-hp-y2z63av.local>
2024-07-08 13:50:33 -0600

    New backup 2024-07-08 13:50:33

--- startall.sh
+++ startall.sh
@@ -5,7 +5,7 @@ script_root="$( dirname $0 )"
 for y in "${script_root}"/*.yml;do
   by="$(basename $y)"
   sudo docker-compose -f "${y}" pull
-  sudo docker-compose -f "${y}" -p "${by}" up -d
+  sudo docker-compose -f "${y}" -p "${by//.}" up -d
 done
 
 sudo docker image prune -f
